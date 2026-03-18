f4472abfa6ae0a82cc4ff32489a2196c6d9a81b1
Blankettechnologiespvtltd <amit.kumar@blanket-technologies.com>
2026-03-18 13:21:05 +0530

    add cryptography dependency

--- contact_app/admin.py
+++ contact_app/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from .models import Contact
 
-
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'mobile', 'region', 'created_at')
     search_fields = ('name', 'email', 'mobile')
     list_filter = ('region', 'created_at')
-
+    ordering = ('-created_at',)
+    list_per_page = 10
 
 admin.site.register(Contact, ContactAdmin)
--- contact_app/urls.py
+++ contact_app/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import ContactCreateAPI, ExportExcelAPI, home
+from rest_framework.routers import DefaultRouter
+from .views import ContactCreateAPI, ExportExcelAPI, home, ContactViewSet
+
+router = DefaultRouter()
+router.register(r'contacts', ContactViewSet)
 
 urlpatterns = [
     path('register/', ContactCreateAPI.as_view()),
     path('export-excel/', ExportExcelAPI.as_view()),
     path('', home),
 ]
+
+# 👇 IMPORTANT: add this line
+urlpatterns += router.urls
