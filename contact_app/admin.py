a8521e5aa75a2883025527f9cdd77c4642650a61
Blankettechnologiespvtltd <amit.kumar@blanket-technologies.com>
2026-03-23 17:01:07 +0530

    region changed Subject

--- contact_app/admin.py
+++ contact_app/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Contact
 
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'mobile', 'subject', 'created_at')
+    list_display = ('name', 'email', 'mobile', 'subject', 'message', 'created_at')
     search_fields = ('name', 'email', 'mobile')
     list_filter = ('subject', 'created_at')
     ordering = ('-created_at',)
