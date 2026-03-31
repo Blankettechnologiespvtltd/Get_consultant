291970f7baa61e31fafd977a0c95340e8738cea0
Blankettechnologiespvtltd <amit.kumar@blanket-technologies.com>
2026-03-31 17:18:41 +0530

    do not repeat mobile number

--- contact_app/models.py
+++ contact_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    mobile = models.CharField(max_length=10)
+    mobile = models.CharField(max_length=10,unique=True)
     subject = models.CharField(max_length=100)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
